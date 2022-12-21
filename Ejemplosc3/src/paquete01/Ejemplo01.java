d0db4e19a1a2baf5e0a28f98ae4b845f8caf885e
Dara <dvan1@utpl.edu.ec>
2022-12-21 09:49:05 -0500

    Primer commit

--- Ejemplosc3/src/paquete01/Ejemplo01.java
+++ Ejemplosc3/src/paquete01/Ejemplo01.java
@@ -23,6 +23,10 @@ public class Ejemplo01 {
         // forma 2
         int[][] arreglo2 = {{10, 20, 30}, {100, 200, 300}};
         
+        //cadena
+        String [][] arregloCadena = {{"Ana", "Jose", "Pedro" },
+            {"Juana", "Maria", "David"}};
+        
         // forma 3
         int[][] arreglo3 = new int[3][];
         arreglo3[0] = new int[2];
